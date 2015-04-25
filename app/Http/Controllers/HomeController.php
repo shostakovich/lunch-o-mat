10b1ee30eb837084c6c50fe4aa13aa9693c08c0b
Robert Curth <robert@rocu.de>
2015-04-25 09:58:08 +0200

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php namespace App\Http\Controllers;
+
+class HomeController extends Controller {
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	public function index()
+	{
+		return view('home');
+	}
+}
