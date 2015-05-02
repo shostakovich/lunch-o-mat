86afbb861bda6862409ae07461d8b24f1fdcd4a9
Robert Curth <robert@rocu.de>
2015-05-02 16:32:14 +0200

    Schedule lunches for any size of group

--- app/LunchScheduler.php
+++ app/LunchScheduler.php
@@ -11,21 +11,37 @@ class LunchScheduler {
 
         $participants->shuffle();
         $numParticipants = $participants->count();
+
         if ($numParticipants % 3 == 0)
         {
             for($i=0; $i < ($numParticipants / 3); $i++)
             {
                 $offset = $i*3;
-                $length = ($i+1)*3;
-
-                $groups[$i] = $participants->slice($offset, $length);
+                $groups[$i] = $participants->slice($offset, 3);
             }
         }
         elseif ($numParticipants % 3 == 1)
         {
-            $groups[0] = $participants->slice(0, 2);
-            $groups[1] = $participants->slice(2, 2);
+            $numberOfGroups = intval($numParticipants / 3) + 1;
 
+            for($i=0; $i < $numberOfGroups - 2; $i++)
+            {
+                $offset = $i*3;
+                $groups[$i] = $participants->slice($offset, 3);
+            }
+            $groups[$numberOfGroups - 2] = $participants->slice(-4, 2);
+            $groups[$numberOfGroups - 1] = $participants->slice(-2, 2);
+        }
+        elseif($numParticipants % 3 == 2)
+        {
+            $numberOfGroups = intval($numParticipants / 3) + 1;
+
+            for($i=0; $i < $numberOfGroups - 1; $i++)
+            {
+                $offset = $i*3;
+                $groups[$i] = $participants->slice($offset, 3);
+            }
+            $groups[$numberOfGroups - 1] = $participants->slice(-2, 2);
         }
 
         return $groups;
--- tests/LunchSchedulerTest.php
+++ tests/LunchSchedulerTest.php
@@ -10,17 +10,22 @@ class LunchSchedulerTest extends TestCase
         $this->assertEquals(count($groups), 0);
     }
 
+    public function testOnePersonLandsInOwnGroup()
+    {
+        $groups = $this->scheduleParticipants(1);
+        $this->assertEquals(count($groups[0]), 1);
+    }
+
+    public function testTwoPersonsLandsInOwnGroup()
+    {
+        $groups = $this->scheduleParticipants(2);
+        $this->assertEquals(count($groups[0]), 2);
+    }
+
     public function testThreeParticipantPerGroup()
     {
         $groups = $this->scheduleParticipants(3);
         $this->assertEquals(count($groups[0]), 3);
-    }
-
-    public function testSixParticipantsAreTwoGroups()
-    {
-        $groups = $this->scheduleParticipants(6);
-        $this->assertEquals(count($groups[0]), 3);
-        $this->assertEquals(count($groups[1]), 3);
     }
 
     public function testFourParticipantsAreTwoGroupsOfTwo()
@@ -29,6 +34,24 @@ class LunchSchedulerTest extends TestCase
         $this->assertEquals(count($groups[0]), 2);
         $this->assertEquals(count($groups[1]), 2);
     }
+
+
+    public function testSevenParticipantsAreOneGroupsOfThreeAndTwoOfTwo()
+    {
+        $groups = $this->scheduleParticipants(7);
+        $this->assertEquals(count($groups[0]), 3);
+        $this->assertEquals(count($groups[1]), 2);
+        $this->assertEquals(count($groups[2]), 2);
+    }
+
+
+    public function testSixParticipantsAreTwoGroups()
+    {
+        $groups = $this->scheduleParticipants(6);
+        $this->assertEquals(count($groups[0]), 3);
+        $this->assertEquals(count($groups[1]), 3);
+    }
+
 
     private function scheduleParticipants($numberOfParticipants)
     {
